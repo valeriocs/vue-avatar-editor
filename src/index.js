d29af1055f279d884961c60d5ea1dda46dda877c
Frederic Pluquet <fpluquet@yahoo.fr>
2018-04-05 05:50:40 +0200

    Add index

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import VueAvatar from './components/VueAvatar.vue';
+import VueAvatarEditor from './components/VueAvatarEditor.vue';
+export default VueAvatarEditor;
+
+export { VueAvatar, VueAvatarEditor };
